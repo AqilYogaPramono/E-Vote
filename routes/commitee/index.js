79bc525b221a2d4450f42b079a739a648b6ea7fd
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-02-17 23:32:16 +0700

    feat: added template project with folder structure and comments in files for future features

---
+++ routes/commitee/index.js
@@ -0,0 +1,5 @@
+//get untuk halaman index panitia dan su
+//post post untuk halaman index panitia dan su
+//put untuk halaman index panitia dan su
+//get untuk halaman panitia setting
+//put untuk halaman ubah password panitia
---
+++ routes/landingPagePostVoting/postVoting.js
@@ -0,0 +1,6 @@
+//get untuk halaman index pada saat waktu voting setalh berakhir
+//get untuk halaman statitik berdasarkan tahun angkatan
+//get untuk halaman statitik berdasarkan fakultas
+//get untuk halaman statitik berdasarkan departemen
+//get untuk halaman statitik berdasarkan jurusan
+//get untuk halaman statitik berdasarkan jenis kelamin
